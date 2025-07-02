02bfc2596083018bf4135e97e0b74e692e310baf
kalpesh <kalpesh.zolvac@gmail.com>
2025-07-02 17:08:24 +0530

    Migration permission Bug reolve.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\View;
 use App\Http\View\Composers\HeaderComposer;
 use App\Http\View\Composers\BreadcrumbComposer;
 use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,28 +25,29 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Schema::defaultStringLength(191);
         // Register Header View Composer
         View::composer('components.team.layout.app', TeamAppComposer::class);
 
         View::composer('components.team.auth.branding', TeamAppComposer::class);
-        
+
         // Register Breadcrumb View Composer
         View::composer('team.components.breadcrumbs', BreadcrumbComposer::class);
-        
+
         // Configure authentication redirects based on the request path
         Authenticate::redirectUsing(function ($request) {
             if ($request->is('team') || $request->is('team/*')) {
                 return route('team.login');
             }
-            
+
             if ($request->is('student') || $request->is('student/*')) {
                 return route('team.login'); // Change this when student login is created
             }
-            
+
             if ($request->is('partner') || $request->is('partner/*')) {
                 return route('team.login'); // Change this when partner login is created
             }
-            
+
             // Default redirect
             return route('team.login');
         });
