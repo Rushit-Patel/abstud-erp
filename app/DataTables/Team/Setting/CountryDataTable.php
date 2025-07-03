488784f52a3075cfa7b1b2b06c0f01f7fab758cd
parthzolvac <parth.abstud@gmail.com>
2025-07-03 12:46:49 +0530

    country master modual done using datatable

--- app/DataTables/Team/Setting/CountryDataTable.php
+++ app/DataTables/Team/Setting/CountryDataTable.php
@@ -85,9 +85,8 @@ class CountryDataTable extends DataTable
     // Dummy render methods for customization
     protected function renderAction($row): string
     {
-        $button = '<a href="'.route('team.settings.countries.edit', $row->id).'" class="btn btn-sm btn-primary"><i class="ki-filled ki-notepad-edit me-2"></i></a>';
-        // $delete = '<a href="'.route('team.settings.countries.destroy', $row->id).'" class="btn btn-sm btn-primary">Delete</a>';
-        $deleteBtn = '<button type="button" class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#deleteModal" data-id="' . $row->id . '">Delete</button>';
+        $button = '<a href="'.route('team.settings.countries.edit', $row->id).'" class="btn btn-sm btn-primary"><i class="ki-filled ki-notepad-edit text-2xl me-2"></i></a>';
+        $deleteBtn = '<button type="button" onclick="openDeleteModal(' . $row->id . ')" ><i class="ki-filled ki-trash text-2xl"></i></button>';
         return $button. ' ' .$deleteBtn;
     }
 
--- app/Models/Country.php
+++ app/Models/Country.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Country extends Model
 {
-    use HasFactory;
+    use HasFactory ,SoftDeletes;
 
     protected $fillable = [
         'name',
@@ -19,10 +20,10 @@ class Country extends Model
         'icon',
         'is_active'
     ];
-
     protected $casts = [
         'timezones' => 'array',
         'is_active' => 'boolean',
+        'deleted_at' => 'datetime',
     ];
 
     /**
